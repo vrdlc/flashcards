cfdf559f771e91e209abb98535a3562d585f3c95
Brianna Popson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-11 15:45:31 -0800

    Add usable definition toggles for key words

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#definition").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#definition2").toggle();
+  });
+});
