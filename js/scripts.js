c06e1e5334cea8180b276b1e1b1c62a962b47fd3
Brianna Popson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-11 16:05:13 -0800

    Add whole term list and all toggles

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#definition").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#definition2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#definition3").toggle();
+  });
+  $(".clickable4").click(function() {
+    $("#definition4").toggle();
+  });
+  $(".clickable5").click(function() {
+    $("#definition5").toggle();
+  });
+  $(".clickable6").click(function() {
+    $("#definition6").toggle();
+  });
+  $(".clickable7").click(function() {
+    $("#definition7").toggle();
+  });
+  $(".clickable8").click(function() {
+    $("#definition8").toggle();
+  });
+  $(".clickable9").click(function() {
+    $("#definition9").toggle();
+  });
+  $(".clickable10").click(function() {
+    $("#definition10").toggle();
+  });
+  $(".clickable11").click(function() {
+    $("#definition11").toggle();
+  });
+  $(".clickable12").click(function() {
+    $("#definition12").toggle();
+  });
+  $(".clickable13").click(function() {
+    $("#definition13").toggle();
+  });
+  $(".clickable14").click(function() {
+    $("#definition14").toggle();
+  });
+  $(".clickable15").click(function() {
+    $("#definition15").toggle();
+  });
+  $(".clickable16").click(function() {
+    $("#definition16").toggle();
+  });
+  $(".clickable17").click(function() {
+    $("#definition17").toggle();
+  });
+  $(".clickable18").click(function() {
+    $("#definition18").toggle();
+  });
+
+});
--- scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".clickable1").click(function() {
-    $("#definition").toggle();
-  });
-  $(".clickable2").click(function() {
-    $("#definition2").toggle();
-  });
-});
